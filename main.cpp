ab00997fcd98a9abc13f7d9b10f015d4ff487ae6
neelyp <160043302+neelyp@users.noreply.github.com>
2024-09-10 09:19:48 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
+std::string artist = "carti";
+int rank = 1;
+bool goat = true;
+float albums = 0.5;
 
+int main()
+{
+    std::cout << "the artist is: " << artist << std::endl;
+    if (goat) {
+        std::cout << "Carti is the number " << rank << " artist in the world" << std::endl;
+    }
+
+    std::cout << "carti has dropped " << albums << " albums in the past 4 years." << std::endl;
+}
