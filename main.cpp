228a99ba5361a89c9e78c3728866a70611ec0a98
neelyp <160043302+neelyp@users.noreply.github.com>
2024-10-15 13:22:06 +0000

    printf

--- main.cpp
+++ main.cpp
@@ -1,15 +1,14 @@
 #include <iostream>
-std::string artist = "carti";
-int rank = 1;
-bool goat = true;
-float albums = 0.5;
+#include <string>
+using namespace std;
 
-int main()
-{
-    std::cout << "the artist is: " << artist << std::endl;
-    if (goat) {
-        std::cout << "Carti is the number " << rank << " artist in the world" << std::endl;
-    }
+int main() {
+    string name;
+    int age;
+    char initial;
+    float gradeAverage;
 
-    std::cout << "carti has dropped " << albums << " albums in the past 4 years." << std::endl;
+    printf("Enter your name, age, first name initial, and grade average: ");
+    scanf("%s %d %c %f", &name, &age, &initial, &gradeAverage);
+    printf("Name: %s, Age: %d, Initial: %c, Grade Average: %.2f", name, age, initial, gradeAverage)
 }
