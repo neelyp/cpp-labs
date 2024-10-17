69ed3d0bba43ac1654499c3e691e66cbd5e47224
neelyp <160043302+neelyp@users.noreply.github.com>
2024-10-17 08:06:32 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,12 +3,12 @@
 using namespace std;
 
 int main() {
-    string name;
     int age;
     char initial;
     float gradeAverage;
+    char name[100];
 
     printf("Enter your name, age, first name initial, and grade average: ");
     scanf("%s %d %c %f", &name, &age, &initial, &gradeAverage);
-    printf("Name: %s, Age: %d, Initial: %c, Grade Average: %.2f", name, age, initial, gradeAverage)
+    printf("Name: %s, Age: %d, Initial: %c, Grade Average: %.2f", name, age, initial, gradeAverage);
 }
