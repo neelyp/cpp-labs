9e5095405fd7a24f8744a3765a6899a3a3b28e41
neelyp <neel.pandruvada@mcvts.org>
2024-11-26 18:43:47 -0500

    fix

--- main.cpp
+++ main.cpp
@@ -1,83 +1,27 @@
-// Looping Lab
+// Top x
 // 10/30/24
 // Neel Pandruvada
-// extra: Print energy and suppplies at the end
+// 
 
 #include <iostream>
 #include <string>
 
 int main()
 {
-    // // journal
-    std::string quotes[7];
-    for (int i = 0; i < 7; i++)
-    {
-        std::string quote;
-        std::cout << "Enter a quote: " << std::endl;
-        std::getline(std::cin, quotes[i]);
-    }
+  int amnt;
+  std::cout << "How long is your top shows list? " << std::endl;
+  std::cin >> amnt;
+  std::cin.ignore();
+  std::string shows[amnt];
 
-    for (int i = 0; i < 7; i++) // write out all the quotes with day number
-    {
-        std::cout << i + 1 << ": " << quotes[i] << std::endl;
-    }
+  for (int i = 0; i < amnt; i++) {
+    std::cout << "enter show name: " << std::endl;
+    std::getline(std::cin, shows[i]);
+  }
 
-    // zombie survival
-    std::cout << "----------------" << std::endl;
-    int energy = 5;
-    int supplies = 0;
-
-    while (energy > 0 && supplies < 10) // run until ending requirements
-    {
-        std::cout << "Energy: " << energy << "\nSupplies: " << supplies << std::endl;
-        int choice;
-        std::cout << "1: search for supplies\n2: rest";
-        std::cin >> choice;
-
-        if (choice == 1)
-        {
-            energy -= 1;
-            supplies++;
-        }
-        else
-        {
-            energy += 2;
-        }
-        std::cout << energy << " " << supplies << std::endl;
-    }
-
-    printf("You ended with %d energy and %d supplies\n", energy, supplies);
-
-    if (supplies >= 10)
-    {
-        std::cout << "You survived!" << std::endl;
-    }
-    else
-    {
-        std::cout << "You ran out of energy." << std::endl;
-    }
-
-    std::cout << "----------------" << std::endl;
-
-    // lock
-    int lock = 548;
-    int guess = 0;
-    char again;
-
-    do
-    {
-        std::cout << "Guess the code: " << std::endl;
-        std::cin >> guess;
-
-        std::cout << "Guess again? (y/n): " << std::endl;
-        std::cin >> again;
-    } while (guess != lock && again == 'y'); // run once and then keep going again until code is guessed.
-    if (again != 'y')
-    {
-        std::cout << "The lock remains locked..." << std::endl;
-    }
-    else
-    {
-        std::cout << "You unlocked the lock!" << std::endl;
-    }
+  std::cout << "Your Top " << amnt << " Shows: " << std::endl;
+  for (int i = 0; i < amnt; i++) {
+    std::cout << (i+1) << ": " << shows[i] << std::endl;
+  }
+  return 0;
 }
