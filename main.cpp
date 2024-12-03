e5e4f8234ddc70440b1bafeeccc324c1796a9701
neelyp <neel.pandruvada@mcvts.org>
2024-12-03 08:56:30 -0500

    commentos

--- main.cpp
+++ main.cpp
@@ -2,7 +2,6 @@
   Neel Pandruvada
   12/3/24
   Math Problems
-  Extra: 
 */
 
 
@@ -14,15 +13,15 @@ double two();
 int three();
 
 int one() {
-  return abs(10-12);
+  return abs(10-12); // return absolute value of 10-12 because john lost his apples :(
 }
 
 double two() {
-  return log(7);
+  return log(7); // return the natural log of 7 to find the exponent
 }
 
 int three() {
-  return sqrt(25);
+  return sqrt(25); // find square root of 25
 }
 
 int main() {
