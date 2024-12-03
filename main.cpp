006e615a5cb565fd90faa034fcf46e95edaaf002
neelyp <neel.pandruvada@mcvts.org>
2024-12-03 08:52:54 -0500

    finish

--- main.cpp
+++ main.cpp
@@ -1,60 +1,35 @@
 /*
   Neel Pandruvada
-  11/27/2024
-  Pizza Pizza
-  Extra: used pointers to return array
+  12/3/24
+  Math Problems
+  Extra: 
 */
 
 
 #include <iostream>
+#include <cmath>
 
-double calcPrice(char size, int toppings, double* prices);
-void bill(double price, char size, int toppings, double* prices);
-double* prices(char size);
+int one();
+double two();
+int three();
 
-double* prices(char size) {
-  double suzzPruzz = (size == 's') ? 9.99 : 14.99;
-  double tuzzUnitPruzz = (size == 's') ? 1.20 : 1.50;
-
-  double* prices_ = new double[2];
-  prices_[0] = suzzPruzz;
-  prices_[1] = tuzzUnitPruzz;
-
-  return prices_;
+int one() {
+  return abs(10-12);
 }
 
-double calcPrice(char size, int toppings, double* prices) {
-  double suzzPruzz = prices[0];
-  double tuzzUnitPruzz = prices[1];
-  double tuzzPruzz = tuzzUnitPruzz * toppings;
-
-  return tuzzPruzz + suzzPruzz;
+double two() {
+  return log(7);
 }
 
-void bill(double price, char size, int toppings, double* prices) {
-  std::cout << "Size of pizza: " << size << "(" << prices[0] << ")" << std::endl;
-  std::cout << "Number of toppings: " << toppings << "(" << prices[1] << " per topping)" << std::endl;
-  std::cout << "Total price: " << price << std::endl;
+int three() {
+  return sqrt(25);
 }
 
 int main() {
-  std::cout << "Welcome to Athuzz Uzz Puzz!" << std::endl;
+  std::cout << "John has 10 apples. He loses 12. What is the absolute value of his apples?\nJohn has " << one() << " apples" << std::endl;
+  std::cout << "John is in math class. His test says that e^x=7. What is x?\nx = " << two() << std::endl;
+  std::cout << "Atharva Tiggly Tafa gets his test back, but his score says it is the absolute value of the square root of 25. What percent did he get??????\n" << one() << "%" << std::endl;
 
-  char size;
-  int toppings;
-
-  std::cout << "What size pizza do you want? (s/l): " << std::endl;
-  std::cin >> size;
-
-  std::cout << "How many toppings do you want? " << std::endl;
-  std::cin >> toppings;
-
-  double* pizzaPrices = prices(size);
-  double totalPrice = calcPrice(size, toppings, pizzaPrices);
-
-  bill(totalPrice, size, toppings, pizzaPrices);
-
-  delete[] pizzaPrices;
 
   return 0;
 }
