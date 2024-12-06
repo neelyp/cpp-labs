2908c918200887e6f887a31a3ac86585cecd2d4d
neelyp <neel.pandruvada@mcvts.org>
2024-12-06 09:16:53 -0500

    finish

--- main.cpp
+++ main.cpp
@@ -1,34 +1,34 @@
 /*
   Neel Pandruvada
-  12/3/24
-  Math Problems
-*/
-
+  Character Functions
+  12/6/24
+ */
 
 #include <iostream>
-#include <cmath>
-
-int one();
-double two();
-int three();
-
-int one() {
-  return abs(10-12); // return absolute value of 10-12 because john lost his apples :(
-}
-
-double two() {
-  return log(7); // return the natural log of 7 to find the exponent
-}
-
-int three() {
-  return sqrt(25); // find square root of 25
-}
+#include <cctype>
 
 int main() {
-  std::cout << "John has 10 apples. He loses 12. What is the absolute value of his apples?\nJohn has " << one() << " apples" << std::endl;
-  std::cout << "John is in math class. His test says that e^x=7. What is x?\nx = " << two() << std::endl;
-  std::cout << "Atharva Tiggly Tafa gets his test back, but his score says it is the absolute value of the square root of 25. What percent did he get??????\n" << one() << "%" << std::endl;
+  std::string name;
+  bool nums = false;
+  std::string upper;
 
+  std::cout << "¿Cómo te llamas? " << std::endl;
+  std::cin >> name;
+
+  for (char c : name) {
+    if (!isalpha(c)) {
+      nums = true;
+      break;
+    } else {
+      upper += toupper(c);
+    }
+  }
+
+  if (!nums) {
+    std::cout << upper << std::endl;
+  } else {
+    std::cout << "That has numbers or special characters";
+  }
 
   return 0;
 }
