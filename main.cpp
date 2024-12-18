827c10f3909b9f721d7dcfd6d9fa1d587fe60d5d
neelyp <neel.pandruvada@mcvts.org>
2024-12-18 08:52:19 -0500

    changed random

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int main() {
     std::cout << rounded << std::endl;
 
     std::srand(time(0));
-    std::cout << std::rand() % 11 << std::endl; // we need time for random numbers because in order to get different numbers everytime, it needs to get seeded using time.
+    std::cout << (std::rand() % 10) + 1 << std::endl; // we need time for random numbers because in order to get different numbers everytime, it needs to get seeded using time.
 
     return 0;
 }
