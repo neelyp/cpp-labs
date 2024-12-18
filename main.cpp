0b0e98d57e2e4f270f60e5925b48d3425cbe3f9b
neelyp <neel.pandruvada@mcvts.org>
2024-12-18 08:24:14 -0500

    ans question

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int main() {
     std::cout << rounded << std::endl;
 
     std::srand(time(0));
-    std::cout << std::rand() % 11 << std::endl;
+    std::cout << std::rand() % 11 << std::endl; // we need time for random numbers because in order to get different numbers everytime, it needs to get seeded using time.
 
     return 0;
 }
