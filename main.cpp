ac70d5b108522759d33d8ae64b52291ed2e19d35
neelyp <neel.pandruvada@mcvts.org>
2025-01-27 08:34:25 -0500

    a

--- main.cpp
+++ main.cpp
@@ -8,52 +8,61 @@
 #include <cmath>
 #include <iomanip>
 
-float avg(float ar[]);
+float avg(float *ar, int size);
 
-int main() {
+int main()
+{
     // 1
     int rands[10];
     int sum = 0;
     int evens = 0;
     srand(time(0));
 
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < 10; i++)
+    {
         rands[i] = 1 + (rand() % 10);
     }
 
-    for (int num : rands) {
+    for (int num : rands)
+    {
         std::cout << num << " ";
         sum += num;
-        if (num % 2 == 0) evens++;
+        if (num % 2 == 0)
+            evens++;
     }
     std::cout << std::endl;
 
-    for (int i = 9; i >= 0; i--) {
+    for (int i = 9; i >= 0; i--)
+    {
         std::cout << rands[i] << " ";
     }
     std::cout << std::endl;
 
     std::cout << "Evens: " << evens << std::endl;
 
-
-
     std::cout << "Sum: " << sum << std::endl;
 
     // 2
     float sales[7] = {100.50, 200.75, 150.25, 175.00, 220.50, 180.00, 190.25};
-    while (true) {
+    while (true)
+    {
         std::cout << "Do you want to view the sales for a day? (y/n): " << std::endl;
         char choice;
         std::cin >> choice;
-        if (choice == 'n') break;
-        else {
+        if (choice == 'n')
+            break;
+        else
+        {
             std::cout << "Enter the day number (1 for Monday... 7 for Sunday) " << std::endl;
             int day;
             std::cin >> day;
 
-            if (day < 1 || day > 7) {
+            if (day < 1 || day > 7)
+            {
                 std::cout << "not a day" << std::endl;
-            } else {
+            }
+            else
+            {
                 std::cout << "Sales for day: $" << std::fixed << std::setprecision(2) << sales[day - 1] << std::endl;
             }
         }
@@ -62,12 +71,14 @@ int main() {
     // 3
     int ages[20];
     int over15 = 0;
-    for (int i = 0; i < 20; i ++) {
+    for (int i = 0; i < 20; i++)
+    {
         std::cout << "Enter the age for person " << i + 1 << std::endl;
         std::cin >> ages[i];
     }
 
-    for (int num : ages) {
+    for (int num : ages)
+    {
         std::cout << num << " ";
         over15 += num > 15;
     }
@@ -77,19 +88,21 @@ int main() {
 
     // 4
     float finalGrades[6] = {85.8, 93.7, 76, 88.5, 100, 91.3};
-    for (float num : finalGrades) {
+    for (float num : finalGrades)
+    {
         std::cout << num << " ";
     }
     std::cout << std::endl;
-    std::cout << "Average: " << avg(finalGrades) << std::endl;
+    std::cout << "Average: " << avg(finalGrades, 6) << std::endl;
 
     return 0;
 }
 
-float avg(float ar[]) {
+float avg(float *ar, int size)
+{
     float sum = 0;
-    int size = sizeof(ar) / sizeof(ar[0]);
-    for (int i = 0; i < size; i++) {
+    for (int i = 0; i < size; i++)
+    {
         sum += ar[i];
     }
 
