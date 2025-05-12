0da002b766c383fabea93597bc2c652cc64a10da
neelyp <neel.pandruvada@mcvts.org>
2025-05-12 09:09:51 -0400

    finish

--- main.cpp
+++ main.cpp
@@ -1,151 +1,83 @@
-// neel pandruvada
-// 02/12/25
-// extra: used header file to init functions
-
 #include <iostream>
-#include <iomanip>
-#include "main.h"
+#include <cctype>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+#include <vector>
+
+void lowerto(std::string &str);
+void doublee(int &num);
+int addRandom(int num);
+void addInts(std::vector<int> &vec);
 
 int main()
 {
-    srand(time(0));
-    matrix();
-    sumnavg();
-    grades();
+    std::srand(std::time(0));
+
+    std::cout << "gimme a string: " << std::endl;
+    std::string str;
+    std::getline(std::cin, str);
+    std::cout << str << " ";
+    lowerto(str);
+    std::cout << "lowercase: " << str << std::endl;
+
+    std::cout << "gimme a number: " << std::endl;
+    int num;
+    std::cin >> num;
+    std::cout << num << " ";
+    doublee(num);
+    std::cout << "doubled: " << num << std::endl;
+
+    std::cout << "gimme number: " << std::endl;
+    std::cin >> num;
+    std::cout << num << " ";
+    num = addRandom(num);
+    std::cout << "random added: " << num << std::endl;
+
+    std::cout << "gimme three numbers: " << std::endl;
+    std::vector<int> vec;
+    int num1, num2, num3;
+    std::cin >> num1 >> num2 >> num3;
+    vec.push_back(num1);
+    vec.push_back(num2);
+    vec.push_back(num3);
+    for (int i = 0; i < vec.size(); i++)
+    {
+        std::cout << vec[i] << " ";
+    }
+
+    std::cout << std::endl;
+    addInts(vec);
+
+    for (int i = 0; i < vec.size(); i++)
+    {
+        std::cout << vec[i] << " ";
+    }
+    std::cout << std::endl;
+
     return 0;
 }
 
-void matrix()
+void lowerto(std::string &str) // get reference of string
 {
-    std::cout << "How many rows and colums? (1 <= num <= 5)" << std::endl;
-    int rows, cols;
-    std::cin >> rows >> cols;
-    if ((rows >= 1 && rows <= 5) && (cols >= 1 && cols <= 5))
+    for (int i = 0; i < str.length(); i++)
     {
-        int matrix[rows][cols];
-        for (int i = 0; i < rows; i++)
-        {
-            for (int j = 0; j < cols; j++)
-            {
-                // random number 1-50
-                matrix[i][j] = rand() % 50 + 1;
-            }
-        }
-
-        std::cout << "Matrix:" << std::endl;
-        for (int i = 0; i < rows; i++)
-        {
-            for (int j = 0; j < cols; j++)
-            {
-                std::cout << std::setw(4);
-                std::cout << matrix[i][j] << " ";
-            }
-            std::cout << std::endl;
-        }
-
-        // find biggest number
-        int biggest = matrix[0][0];
-        int pos[2];
-        for (int i = 0; i < rows; i++)
-        {
-            for (int j = 0; j < cols; j++)
-            {
-                if (matrix[i][j] > biggest)
-                {
-                    biggest = matrix[i][j];
-                    pos[0] = i;
-                    pos[1] = j;
-                }
-            }
-        }
-
-        std::cout << "Biggest number: " << biggest << std::endl;
-        std::cout << "Position: " << pos[0] << ", " << pos[1] << std::endl;
+        str[i] = tolower(str[i]); // update each character in the string
     }
-    else
-    {
-        std::cout << "Invalid input" << std::endl;
-    }
+} // no need to return as the string is passed by reference so it is automatically updated
+
+void doublee(int &num)
+{
+    num *= 2;
 }
 
-void sumnavg()
+int addRandom(int num)
 {
-    std::cout << "Enter a number between 1 and 10 (inclusive)" << std::endl;
-    int size_;
-    std::cin >> size_;
-    if (size_ >= 1 && size_ <= 10)
-    {
-        int arr[size_][size_];
-        std::cout << "Matrix:" << std::endl;
-        for (int i = 0; i < size_; i++)
-        {
-            for (int j = 0; j < size_; j++)
-            {
-                arr[i][j] = rand() % 100 + 1;
-                std::cout << std::setw(4) << arr[i][j] << " ";
-            }
-            std::cout << std::endl;
-        }
-
-        // sum and avg of each row
-        for (int i = 0; i < size_; i++)
-        {
-            int sum = 0;
-            for (int j = 0; j < size_; j++)
-            {
-                sum += arr[i][j];
-            }
-            std::cout << "Sum of row " << i << ": " << sum << std::endl;
-            std::cout << "Average of row " << i << ": " << std::fixed << std::setprecision(2) << static_cast<double>(sum) / size_ << "\n\n";
-        }
-    }
+    return num += std::rand() % 11;
 }
 
-void grades()
+void addInts(std::vector<int> &vec)
 {
-    std::cout << "Enter the number of students (1-10): ";
-    int students;
-    std::cin >> students;
-
-    if (students < 1 || students > 10)
-    {
-        std::cout << "invalid num" << std::endl;
-        return;
-    }
-
-    int scores[students][3];
-
-    for (int i = 0; i < students; ++i)
-    {
-        for (int j = 0; j < 3; ++j)
-        {
-            scores[i][j] = rand() % 51 + 50;
-        }
-    }
-
-    std::cout << "Math\tEnglish\tScience" << std::endl;
-    for (int i = 0; i < students; ++i)
-    {
-        for (int j = 0; j < 3; ++j)
-        {
-            std::cout << scores[i][j] << "\t";
-        }
-        std::cout << std::endl;
-    }
-
-    double avgMath = 0, avgEnglish = 0, avgScience = 0;
-    for (int i = 0; i < students; ++i)
-    {
-        avgMath += scores[i][0];
-        avgEnglish += scores[i][1];
-        avgScience += scores[i][2];
-    }
-    avgMath /= students;
-    avgEnglish /= students;
-    avgScience /= students;
-
-    std::cout << "\nAverage Scores:" << std::endl;
-    std::cout << "Math: " << std::fixed << std::setprecision(2) << avgMath << std::endl;
-    std::cout << "English: " << std::fixed << std::setprecision(2) << avgEnglish << std::endl;
-    std::cout << "Science: " << std::fixed << std::setprecision(2) << avgScience << std::endl;
+    vec.push_back(1);
+    vec.push_back(2);
 }
--- main.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-void matrix();
-void sumnavg();
-void grades();
